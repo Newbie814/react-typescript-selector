7c47db49d087d8169ff364b73ee63bed48afd865
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-09-23 14:09:43 -0400

    adding event listeners and select item functions

--- src/Select.tsx
+++ src/Select.tsx
@@ -15,19 +15,44 @@ type SelectProps = {
 export function Select({ value, onChange, options }: SelectProps) {
   const [isOpen, setIsOpen] = useState(false);
 
+  function clearOptions() {
+    onChange(undefined);
+  }
+
+  function selectOption(option: SelectOption) {
+    onChange(option);
+  }
+
   return (
     <div
+      onBlur={() => setIsOpen(false)}
       onClick={() => setIsOpen((prev) => !prev)}
       tabIndex={0}
       className={styles.container}
     >
       <span className={styles.value}>{value?.label}</span>
-      <button className={styles['clear-btn']}>&times;</button>
+      <button
+        onClick={(e) => {
+          e.stopPropagation();
+          clearOptions();
+        }}
+        className={styles['clear-btn']}
+      >
+        &times;
+      </button>
       <div className={styles.divider}></div>
       <div className={styles.caret}></div>
       <ul className={`${styles.options} ${isOpen ? styles.show : ''}`}>
         {options.map((option) => (
-          <li key={option.label} className={styles.option}>
+          <li
+            onClick={(e) => {
+              e.stopPropagation();
+              selectOption(option);
+              setIsOpen(false);
+            }}
+            key={option.label}
+            className={styles.option}
+          >
             {option.label}
           </li>
         ))}
